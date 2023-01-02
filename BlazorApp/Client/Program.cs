f2be70812c3352e0a3e0b8dc13d5de30be2a222c
Hussein Beygi <hussein1997.hb@hotmail.com>
2023-01-02 20:14:17 +0330

    Added Blazor for demo

--- BlazorApp/Client/Program.cs
+++ BlazorApp/Client/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
 
 namespace BlazorApp;
 
@@ -15,6 +17,27 @@ public class Program
 
         services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        await builder.Build().RunAsync();
+        builder.Services.AddLocalization();
+
+        var host = builder.Build();
+
+        CultureInfo culture;
+        var js = host.Services.GetRequiredService<IJSRuntime>();
+        var result = await js.InvokeAsync<string>("blazorCulture.get");
+
+        if (result != null)
+        {
+            culture = new CultureInfo(result);
+        }
+        else
+        {
+            culture = new CultureInfo("en-US");
+            await js.InvokeVoidAsync("blazorCulture.set", "en-US");
+        }
+
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+        await host.RunAsync();
     }
 }
